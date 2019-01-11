8ba6b12179eaf9481e2c6ee86b8592909def0601
Juan Carlos Garduño Vargas <juuanc_vargas@ciencias.unam.mx>
2019-01-10 21:08:23 -0600

    Roaming Romans

---
+++ Romans.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.math.*;
+
+public class Romans{
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		double n = sc.nextDouble();
+		double res = n*(1000*5280/4854);
+		BigDecimal big = new BigDecimal(res);
+		big = big.setScale(4, RoundingMode.HALF_UP);
+		int d2 = big.intValue();
+		d2 += 0.5;
+
+		System.out.print(d2);
+	}
+}
