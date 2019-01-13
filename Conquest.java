b77b709635bf49ea1f0cee196b50e5ea3924bde7
Juan Carlos Garduño Vargas <juuanc_vargas@ciencias.unam.mx>
2019-01-13 01:42:58 -0600

    Conquest second commit

--- Conquest.java
+++ Conquest.java
@@ -7,24 +7,38 @@ public class Conquest{
 		int r = sc.nextInt();
 		int c = sc.nextInt();
 		int n = sc.nextInt();
+		int cont = 0;
+		int days = 1;
 		int[][] tablero = new int[r][c];
 
 		for(int i = 0; i < tablero.length; i++){
 			for(int j = 0; j < tablero[0].length; j++){
 				tablero[i][j] = 0;
+				cont++;
 			}
 		}
 
 		for(int i = 0; i < n; i++){
 			int x = sc.nextInt();
 			int y = sc.nextInt();
-			tablero[x-1][y-1] = 1;
+			tablero[x-1][y-1] = 2;
+
 		}
 
+		//En vez de cambiar a uno suma un valor despues checas si los valores son mayores o igual a uno para verificar si ya fueron visitados
 		for(int i = 0; i < tablero.length; i++){
 			for(int j = 0; j < tablero[0].length; j++){
-				if(tablero[i][j] == 1){
-					tablero[][]
+				if(tablero[i][j] == 2){
+					if(j+1 < tablero[i].length && tablero[i][j+1] == 0){
+						tablero[i][j+1] = 1;
+					}
+					if(j-1 < 0){
+						j = 0;
+					} 
+					if(j < tablero[i].length && tablero[i][j-1] == 0){
+						tablero[i][j-1] = 1;
+					}
+					cont--;
 				}
 			}
 			
@@ -36,5 +50,25 @@ public class Conquest{
 			}
 			System.out.println();
 		}
+		/*
+		if(cont == 0){
+			System.out.print(days);
+		}else{
+			visit(tablero);
+			days++;
+		}*/
 	}
+
+	/*public static void visit(int[][] tablero){
+		for(int i = 0; i < tablero.length; i++){
+			for(int j = 0; j < tablero[0].length; j++){
+				if(tablero[i][j] == 1){
+					if(j+1 < tablero[i].length && tablero[i][j+1] ){
+
+					}
+				}
+			}
+			
+		}
+	} */
 }
