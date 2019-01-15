741b0fa0a0c714b134e57ae88837420191f124eb
Juan Carlos Garduño Vargas <juuanc_vargas@ciencias.unam.mx>
2019-01-14 21:34:10 -0600

    Conquest solved

--- Conquest.java
+++ Conquest.java
@@ -1,80 +1,76 @@
 import java.util.*;
 
 public class Conquest{
+
 	public static void main(String args[]){
 		Scanner sc = new Scanner(System.in);
 
 		int r = sc.nextInt();
 		int c = sc.nextInt();
 		int n = sc.nextInt();
-		int cont = 0;
-		int days = 1;
+		int cont = 1;
+		int zeros = 0;
+		int cuadros = 0;
+		int val = 1;
 		int[][] tablero = new int[r][c];
 
 		for(int i = 0; i < tablero.length; i++){
 			for(int j = 0; j < tablero[0].length; j++){
 				tablero[i][j] = 0;
-				cont++;
 			}
 		}
 
 		for(int i = 0; i < n; i++){
 			int x = sc.nextInt();
 			int y = sc.nextInt();
-			tablero[x-1][y-1] = 2;
+			tablero[x-1][y-1] = val;
 
 		}
 
-		//En vez de cambiar a uno suma un valor despues checas si los valores son mayores o igual a uno para verificar si ya fueron visitados
-		for(int i = 0; i < tablero.length; i++){
-			for(int j = 0; j < tablero[0].length; j++){
-				if(tablero[i][j] == 2){
-					if(j+1 < tablero[i].length && tablero[i][j+1] == 0){
-						tablero[i][j+1] = 1;
-					}
+		zeros = days(tablero, val);
 
-					if(tablero[i][j-1] < tablero[i].length && tablero[i][j-1] == 0){
-						tablero[i][j-1] = 1;
-					}
-					if(i+1 < tablero[i].length && tablero[i+1][j] == 0){
-						tablero[i+1][j] = 1;
-					}
-
-					if(i-1 < tablero[i].length && tablero[i-1][j] == 0){
-						tablero[i-1][j] = 1;
-					}
-
-					cont--;
-				}
-			}
-			
+		while(zeros != 0){
+			zeros = days(tablero, val);
+			cont++;
+			val++;
 		}
+		System.out.println(cont);
 
-		for(int i = 0; i < tablero.length; i++){
-			for(int j = 0; j < tablero[0].length; j++){
-				System.out.print(tablero[i][j]);
-			}
-			System.out.println();
-		}
-		/*
-		if(cont == 0){
-			System.out.print(days);
-		}else{
-			visit(tablero);
-			days++;
-		}*/
 	}
 
-	/*public static void visit(int[][] tablero){
+	public static int days(int[][] tablero, int val){
+
+		int ind = 0;
+
 		for(int i = 0; i < tablero.length; i++){
 			for(int j = 0; j < tablero[0].length; j++){
-				if(tablero[i][j] == 1){
-					if(j+1 < tablero[i].length && tablero[i][j+1] ){
-
+				if(tablero[i][j] == val){
+					if(j+1 < tablero[i].length && tablero[i][j+1] == 0){
+						tablero[i][j+1] = val + 1;
+					}
+					if(j-1 >= 0 && tablero[i][j-1] < tablero[i].length && tablero[i][j-1] == 0){
+						tablero[i][j-1] = val + 1;
+					}
+					if(i-1 >= 0 && tablero[i-1][j] < tablero[i].length && tablero[i-1][j] == 0){
+						tablero[i-1][j] = val + 1;
+					}
+					
+					if(i+1 < tablero.length && tablero[i+1][j] == 0){
+						tablero[i+1][j] = val + 1;
 					}
 				}
-			}
-			
+			}		
 		}
-	} */
+
+		for(int i = 0; i < tablero.length; i++){
+			for(int j = 0; j < tablero[0].length; j++){
+				if(tablero[i][j] == 0){
+					ind++;
+				}
+			}
+		}
+
+		return ind;
+
+	}
 }
