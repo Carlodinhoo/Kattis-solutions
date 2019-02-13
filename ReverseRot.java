3acb5cacf24535fbb10a8390a4ee69b36c273f95
Juan Carlos Garduño Vargas <juuanc_vargas@ciencias.unam.mx>
2019-02-13 00:09:55 -0600

    Reverse Rot

--- ReverseRot.java
+++ ReverseRot.java
@@ -3,23 +3,35 @@ import java.util.*;
 public class ReverseRot{
 	public static void main(String args[]){
 		Scanner sc = new Scanner(System.in);
-		String cadena = sc.nextLine();
-		int ext = sc.nextInt();
-		String reverse = "";
-		/*
-		for(int i = cadena.length()-1; i >= 0; i--){
-			int number = (int) cadena.charAt(i);
-			number += ext;
-			char a = (char) number;
-			reverse += a;
-		}*/
-		for(int i = cadena.length()-1; i >= 0; i--){
-			int num = (int) cadena.charAt(i);
-			int aux = (num-65+ext)%28+65;
-			char a = (char) aux;
-			reverse += a;
-			System.out.println(aux);
+		int ext;
+		while(true){
+			ext = sc.nextInt();
+
+			if(ext == 0){
+				break;
+			}
+
+			String cadena = sc.nextLine().substring(1);
+			String reverse = "";
+
+			for(int i = cadena.length()-1; i >= 0; i--){
+				int num = (int) cadena.charAt(i);
+				if(num == 95){
+					num = 91;
+				}else if(num == 46){
+					num = 92;
+				}
+				int aux = (num-65+ext)%28+65; //Movimiento clave
+				if(aux == 91){
+					aux = 95;
+				}else if(aux == 92){
+					aux = 46;
+				}
+				char a = (char) aux;
+				reverse += a;
+			}
+			System.out.println(reverse);
 		}
-		System.out.println(reverse);
+		
 	}
 }
